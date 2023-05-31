8bced50f54fd51827d4bda934c8aee6b213bf428
chanhuirobot <chanhui.robot@gmail.com>
2023-05-31 14:50:23 +0900

    카메라 노드 수정

--- rh_plus_2_axis_manipulator_code/src/camera/usb_camera_driver.cpp
+++ rh_plus_2_axis_manipulator_code/src/camera/usb_camera_driver.cpp
@@ -13,9 +13,9 @@ CameraDriver::CameraDriver(const rclcpp::NodeOptions &node_options) : Node("usb_
     // 파라미터 선언
     frame_id_ = this->declare_parameter("frame_id", "camera");
     // parameter 이름 : my_parameter, 기본값 world
-    image_width_ = this->declare_parameter("image_width", 640);
-    image_height_ = this->declare_parameter("image_height", 480);
-    fps_ = this->declare_parameter("fps", 10.0);
+    image_width_ = this->declare_parameter("image_width", 1280);
+    image_height_ = this->declare_parameter("image_height", 720);
+    fps_ = this->declare_parameter("fps", 2.0);
 
     camera_id = this->declare_parameter("camera_id", 0);
 
@@ -27,7 +27,9 @@ CameraDriver::CameraDriver(const rclcpp::NodeOptions &node_options) : Node("usb_
     cinfo_manager_ = std::make_shared<camera_info_manager::CameraInfoManager>(this);
 
     /* get ROS2 config parameter for camera calibration file */
-    auto camera_calibration_file_param_ = this->declare_parameter("camera_calibration_file", "file://config/camera.yaml");
+    std::string file_path;
+    file_path = "file:///home/" + std::string(getenv("USER")) + "/.ros/config/camera.yaml";
+    auto camera_calibration_file_param_ = this->declare_parameter("camera_calibration_file", file_path);
     cinfo_manager_->loadCameraInfo(camera_calibration_file_param_);
 
     // 비디오 캡쳐 객체 다루기
@@ -124,4 +126,3 @@ void CameraDriver::ImageCallback()
         camera_info_pub_.publish(image_msg_, camera_info_msg_);
     }
 }
-
