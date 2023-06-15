4e5d88edad2700257f5b0ddf4ce78feb655905ba
Poodlee <ppp6131@yonsei.ac.kr>
2023-06-15 15:33:46 +0900

    launch 파일 수정중

---
+++ rh_plus_2_axis_manipulator_code/launch/launch_raspberrypi.py
@@ -0,0 +1,29 @@
+
+import os
+
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='rh_plus_2_axis_manipulator_code',
+            namespace='camera',
+            executable='camera_node',
+            name='camera',
+            # output='screen',
+            # prefix='lxterminal -e'
+        ),
+        Node(
+            package='rh_plus_2_axis_manipulator_code',
+            namespace='servo_control',
+            executable='servo_control_node',
+            name='servo_control',
+            # output='screen',
+            # prefix='lxterminal -e'
+        )
+    ])
