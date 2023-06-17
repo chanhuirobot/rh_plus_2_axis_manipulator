e48423235d0a50a97e01a9d9ccd0523a1a14232e
chanhuirobot <chanhui.robot@gmail.com>
2023-06-17 23:31:20 +0900

    으아아아악 감사합니다! 완성!!!!!!

--- rh_plus_2_axis_manipulator_code/src/camera/vision_process_node.cpp
+++ rh_plus_2_axis_manipulator_code/src/camera/vision_process_node.cpp
@@ -5,7 +5,7 @@ VisionNode::VisionNode() : Node("vision_node")
 {
   // QoS 설정, 스브스크라이버 설정
   const auto QOS_RKL10V =
-    rclcpp::QoS(rclcpp::KeepLast(5)).best_effort().durability_volatile();
+    rclcpp::QoS(rclcpp::KeepLast(2)).best_effort().durability_volatile();
   image_subscriber_ = this->create_subscription<sensor_msgs::msg::Image>(
     "/image", QOS_RKL10V, std::bind(&VisionNode::image_process_callback, this, std::placeholders::_1));
 }
@@ -16,8 +16,8 @@ void VisionNode::image_process_callback(sensor_msgs::msg::Image::SharedPtr data)
   cv::Mat img = cv_bridge::toCvShare(data, "bgr8")->image;
 
   // 먼저 이미지 필요없는 부분들 잘라내기(화이트보드만 나오도록)
-  int output_width = 350;
-  int output_height = 350;
+  int output_width = 400;
+  int output_height = 400;
   // Warping 전의 이미지 상의 좌표. 좌상, 우상, 우하, 좌하 (시계 방향으로 4 지점 정의)
   cv::Point2f corners[4] = {cv::Point2f(175, 30), cv::Point2f(445, 32), cv::Point2f(444, 341), cv::Point2f(172, 341)};
   // Warping 후의 좌표
@@ -50,38 +50,42 @@ void VisionNode::image_process_callback(sensor_msgs::msg::Image::SharedPtr data)
   cv::Mat labels, stats, centroids;
   int nlabels = cv::connectedComponentsWithStats(img_end, labels, stats, centroids);
 
-  // 가장 큰 영역의 빨간색 덩어리만 사용하므로
-  double max = -1;
-  int max_index = -1;
-  for(int i = 0; i < nlabels; i++){
-    if(i < 1) continue;
-    double area = stats.at<double>(i, cv::CC_STAT_AREA);
-    if(area > max){
-      max = area;
-      max_index = i;
+  // 만약 빨간점이 발견되면 최신화. 거꾸로 말하면 발견 안되면 최신화 안함.
+  if(nlabels > 1){
+    // 가장 큰 영역의 빨간색 덩어리만 사용하므로
+    double max = -1;
+    int max_index = -1;
+    for(int i = 0; i < nlabels; i++){
+      if(i < 1) continue;
+      double area = stats.at<double>(i, cv::CC_STAT_AREA);
+      if(area > max){
+        max = area;
+        max_index = i;
+      }
     }
+
+    // 가장 큰 영역에서 중심 좌표, 너비, 높이 등을 파악
+    int center_x = int(centroids.at<double>(max_index, 0));
+    int center_y = int(centroids.at<double>(max_index, 1));
+    int left = stats.at<int>(max_index, cv::CC_STAT_LEFT);
+    int top = stats.at<int>(max_index, cv::CC_STAT_TOP);
+    int width = stats.at<int>(max_index, cv::CC_STAT_WIDTH);
+    int height = stats.at<int>(max_index, cv::CC_STAT_HEIGHT);
+
+    // 빨간색 사각형과 초록색 점 그리기
+    cv::rectangle(img_warp, cv::Rect(left, top, width, height), cv::Scalar(0,0,255), 2);
+    cv::circle(img_warp, cv::Point(center_x, center_y), 4, cv::Scalar(0,255,0), -1);
+
+    // 원했던 목표 : 좌표 비율 구하고 적용해주기 -> 최신화 완료
+    coord_x_ratio_ = (double)center_x / (double)output_width;
+    coord_y_ratio_ = (double)(output_height - center_y) / (double)output_height;
   }
-
-  // 가장 큰 영역에서 중심 좌표, 너비, 높이 등을 파악
-  int center_x = int(centroids.at<double>(max_index, 0));
-  int center_y = int(centroids.at<double>(max_index, 1));
-  int left = stats.at<int>(max_index, cv::CC_STAT_LEFT);
-  int top = stats.at<int>(max_index, cv::CC_STAT_TOP);
-  int width = stats.at<int>(max_index, cv::CC_STAT_WIDTH);
-  int height = stats.at<int>(max_index, cv::CC_STAT_HEIGHT);
-
-  // 빨간색 사각형과 초록색 점 그리기
-  cv::rectangle(img_warp, cv::Rect(left, top, width, height), cv::Scalar(0,0,255), 2);
-  cv::circle(img_warp, cv::Point(center_x, center_y), 4, cv::Scalar(0,255,0), -1);
 
   // 화면 출력
   // cv::imshow("original", img); // 원본 화면 : 필요시 활성화
-  cv::imshow("cropped", img_warp);
   // cv::imshow("mask", img_end); // 이미지 후처리 화면 : 필요시 활성화
-
-  // 원했던 목표 : 좌표 비율 구하고 적용해주기 -> 최신화 완료
-  coord_x_ratio_ = (double)center_x / (double)output_width;
-  coord_y_ratio_ = (double)(output_height - center_y) / (double)output_height;
+  cv::imshow("output", img_warp); // 딱 필요한 화면(자르기 완료 + 사각형 점 등 시각처리)
+  cv::waitKey(1);
 }
 
 
@@ -98,8 +102,8 @@ void VisionNode::image_process_callback(sensor_msgs::msg::Image::SharedPtr data)
 
 
 MoveItController::MoveItController() : Node("moveit_node"){
-  // 10초마다 moveit2_controller 발동시키기
-  this->moveit_timer_ = this->create_wall_timer(std::chrono::seconds(10), std::bind(&MoveItController::moveit2_controller, this));
+  // 15초마다 moveit2_controller 발동시키기
+  this->moveit_timer_ = this->create_wall_timer(std::chrono::seconds(15), std::bind(&MoveItController::moveit2_controller, this));
 
 
 }
@@ -108,10 +112,10 @@ MoveItController::MoveItController() : Node("moveit_node"){
 // 안되면 안된다고 에러 띄우기(ex 닿을 수 있는 거리 밖이면)
 void MoveItController::moveit2_controller(){
   // countdown 구현
-  /*for(int countdown_num = 10; countdown_num > 0; countdown_num--){
-    RCLCPP_INFO(get_logger(), "Countdown for next Moving: %ds / current coord ratio : (%.3f,%.3f)", countdown_num, VisionNode::coord_x_ratio_, VisionNode::coord_y_ratio_);
+  for(int countdown_num = 10; countdown_num > 0; countdown_num--){
+    RCLCPP_INFO(get_logger(), "Countdown for next Moving: %ds / current coord ratio : (x = %.3f, y = %.3f)", countdown_num, VisionNode::coord_x_ratio_, VisionNode::coord_y_ratio_);
     rclcpp::sleep_for(std::chrono::seconds(1)); // 1초 딜레이
-  }*/
+  }
 
 
   // 이제 moveit 제어 시작
@@ -121,8 +125,10 @@ void MoveItController::moveit2_controller(){
   target_pose.orientation.w = 1.0;
   target_pose.orientation.z = 0.0;
   // ratio(0.0~1.0) 에다가 길이를 곱해서 target.position 생성
-  target_pose.position.x = VisionNode::coord_x_ratio_ * BOARD_WIDTH;
-  target_pose.position.y = VisionNode::coord_y_ratio_ * BOARD_HEIGHT;
+  double catch_coord_x_ratio = VisionNode::coord_x_ratio_;
+  double catch_coord_y_ratio = VisionNode::coord_y_ratio_;
+  target_pose.position.x = catch_coord_x_ratio * BOARD_WIDTH;
+  target_pose.position.y = catch_coord_y_ratio * BOARD_HEIGHT;
 
   // moveit의 plan 생성
   moveit::planning_interface::MoveGroupInterface::Plan plan;
@@ -131,9 +137,9 @@ void MoveItController::moveit2_controller(){
   auto success = static_cast<bool>(move_group_interface.plan(plan));
   if(success){
     move_group_interface.execute(plan);
-    RCLCPP_INFO(get_logger(), "Success! Coord x = %.3f / y = %.3f", target_pose.position.x, target_pose.position.y);
+    RCLCPP_INFO(get_logger(), "Success! Coord ratio : (x = %.3f, y = %.3f)", catch_coord_x_ratio, catch_coord_y_ratio);
   }
-  else RCLCPP_ERROR(get_logger(), "Planning failed! Maybe Out of Range...! :( Coord x = %.3f / y = %.3f", target_pose.position.x, target_pose.position.y);
+  else RCLCPP_ERROR(get_logger(), "Planning failed! Maybe Out of Range...! :( ratio : (x = %.3f, y = %.3f)", catch_coord_x_ratio, catch_coord_y_ratio);
 }
 
 
