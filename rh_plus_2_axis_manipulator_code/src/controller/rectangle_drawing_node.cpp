7fd401c26a8c5409ebcf3debf2a197522aae8a41
hw5175 <hwjung1006@gmail.com>
2023-06-22 13:42:44 +0900

    현우프로그래밍 완료

--- rh_plus_2_axis_manipulator_code/src/controller/rectangle_drawing_node.cpp
+++ rh_plus_2_axis_manipulator_code/src/controller/rectangle_drawing_node.cpp
@@ -20,9 +20,12 @@ int main(int argc, char* argv[])
   // Target Pose 객체 생성
   geometry_msgs::msg::Pose target_pose;
   // set target pose. w,z는 항상 일정하므로
-  // 여기부터
-
-
+  target_pose.orientation.w = 1.0;
+  target_pose.orientation.z = 0.0;
+  // plan 좌표 입력
+  // 사각형 좌측 하단 이동
+  target_pose.position.x = 0.50 * BOARD_WIDTH;
+  target_pose.position.y = 0.05 * BOARD_HEIGHT;
 
   // moveit의 plan 생성
   moveit::planning_interface::MoveGroupInterface::Plan plan;
@@ -35,20 +38,32 @@ int main(int argc, char* argv[])
   }
 
   // 펜 내리기 위해 5초 대기
+  RCLCPP_INFO(logger, "Start drawing in 5 seconds. Move Pen down.");
+  rclcpp::sleep_for(std::chrono::seconds(5));
 
   // 사각형 그리기 위해 4점 넣기
   std::vector<geometry_msgs::msg::Pose> waypoints;
 
   // 우측 하단
-
+  target_pose.position.x = 0.75 * BOARD_WIDTH;
+  target_pose.position.y = 0.05 * BOARD_HEIGHT;
+  waypoints.push_back(target_pose);
 
   // 우측 상단
+  target_pose.position.x = 0.75 * BOARD_WIDTH;
+  target_pose.position.y = 0.42 * BOARD_HEIGHT;
+  waypoints.push_back(target_pose);
 
 
   // 좌측 상단
-
+  target_pose.position.x = 0.50 * BOARD_WIDTH;
+  target_pose.position.y = 0.42 * BOARD_HEIGHT;
+  waypoints.push_back(target_pose);
 
   // 좌측 하단(약간 밖으로)
+  target_pose.position.x = 0.50 * BOARD_WIDTH;
+  target_pose.position.y = -0.50 * BOARD_HEIGHT;
+  waypoints.push_back(target_pose);
 
   waypoints.push_back(target_pose);
 
